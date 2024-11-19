abb5fbbc6316a46c571ad9ffb40542c50793955b
Pinier-Nicolas <pinier74nicolas@gmail.com>
2024-11-19 19:24:03 +0100

    add strapi lib to use strapi as headless cms

---
+++ src/interfaces/landing.ts
@@ -0,0 +1,13 @@
+export default interface Landing {
+  id: number;
+  attributes: {
+    title: string;
+    logo: string;
+    description: string;
+    company_name: string;
+    main_img: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+  };
+}
